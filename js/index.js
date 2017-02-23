d281e2a90fc6af15e2094d4a29369280ae79f4f8
topiniu <topiniu@outlook.com>
2017-02-23 19:39:50 +0800

    全屏滚动太耗时了！

--- js/index.js
+++ js/index.js
@@ -3,21 +3,53 @@
  */
 $(function(){
     choseCS();
+    addMouseListener();
 });
+var items = ["item1","item2","item3","item4"];
+var itemIndex = 0,length=0,para,flag;
+function onMouseWheel(ev){
+    ev = ev||window.event;
+    flag = true;
+    length = $(".contentContainer").length;
+    flag = ev.wheelDelta?ev.wheelDelta>0:ev.detail<0;
+
+    if(!flag && itemIndex < length){//up
+
+        $(".contentContainer").animate({
+            left: "-=100%"
+        },2000)
+    }else if(flag && itemIndex>0){//down
+
+    }
+    // alert(itemIndex);
+
+}
+
+function addMouseEvent(obj,event,fn){
+    if(obj.attachEvent)
+        obj.attachEvent('on' + event,fn);
+    else
+        obj.addEventListener(event,fn,false);
+}
+function addMouseListener(){
+    var s = document.getElementById("frontContainer");
+    addMouseEvent(s,'mousewheel',onMouseWheel);
+}
 
 var colorClass = ["red","blue","green","gray","orange","yellow","violet"];
 var n = 1,i = 0;
+var row,col,colorNO,cclassName,colorN;
 function choseCS(){
     n = parseInt(Math.random()*36 + 30);
-    var colorN = parseInt(Math.random() * 7, 10);
+    colorN = parseInt(Math.random() * 7, 10);
     for(;i<n;i++) {
         setTimeout(function () {
-            var row = parseInt(Math.random() * 20, 10) + 1;
-            var col = parseInt(Math.random() * 10, 10) + 1;
-            var colorNO = parseInt(Math.random() * 5, 10) + 1;
-            var cclassName = colorClass[colorN] + colorNO;
+            row = parseInt(Math.random() * 20, 10) + 1;
+            col = parseInt(Math.random() * 10, 10) + 1;
+            colorNO = parseInt(Math.random() * 5, 10) + 1;
+            cclassName = colorClass[colorN] + colorNO;
             changeColor(row, col, cclassName);
-        },i*60);
+        },i*120);
 
     }
 
