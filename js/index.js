e617a81c823a11b0f79969b81cfc4f2600d2950d
topiniu <topiniu@outlook.com>
2017-02-24 13:28:08 +0800

    使用全屏滚动插件fullpage.js
    确实很方便啊！！

--- js/index.js
+++ js/index.js
@@ -3,38 +3,8 @@
  */
 $(function(){
     choseCS();
-    addMouseListener();
+    $('#fullpage').fullpage();
 });
-var items = ["item1","item2","item3","item4"];
-var itemIndex = 0,length=0,para,flag;
-function onMouseWheel(ev){
-    ev = ev||window.event;
-    flag = true;
-    length = $(".contentContainer").length;
-    flag = ev.wheelDelta?ev.wheelDelta>0:ev.detail<0;
-
-    if(!flag && itemIndex < length){//up
-
-        $(".contentContainer").animate({
-            left: "-=100%"
-        },2000)
-    }else if(flag && itemIndex>0){//down
-
-    }
-    // alert(itemIndex);
-
-}
-
-function addMouseEvent(obj,event,fn){
-    if(obj.attachEvent)
-        obj.attachEvent('on' + event,fn);
-    else
-        obj.addEventListener(event,fn,false);
-}
-function addMouseListener(){
-    var s = document.getElementById("frontContainer");
-    addMouseEvent(s,'mousewheel',onMouseWheel);
-}
 
 var colorClass = ["red","blue","green","gray","orange","yellow","violet"];
 var n = 1,i = 0;
@@ -55,6 +25,5 @@ function choseCS(){
 
 }
 function changeColor(row,col,cclassName){
-    // alert(col + row + cclassName);
     $(".row:nth-child(" + row + ")>.colorScreen:nth-child(" + col + ")").addClass(cclassName);
 }
